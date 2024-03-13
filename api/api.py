66783f31c5788970e862f01a66c7be5f179e6e43
felipe alvarez <fe.alvarezdiaz@gmail.com>
2024-03-13 10:45:08 -0300

    actulizacion carpeta api

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 import subprocess
 import shutil
+from predict import run_yolov5_detection
 
 app = FastAPI()
 
@@ -8,19 +9,18 @@ app = FastAPI()
 def root():
     return {'estado': 'Funciona'}
 
-@app.post("/predict_video")  # Cambiado a POST para manejar la carga de archivos
+@app.post("/predict_video")  # Cambiado a POST pararun_yolov5_detection manejar la carga de archivos
 async def predict(file: UploadFile = File(...)):
-    local_filename = "video_bonito.mp4"
+    local_filename = "video_bonit.mp4"
     with open(local_filename, 'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     # Asegúrate de que la ruta al modelo y al archivo de entrada sean correctas
-    test = subprocess.Popen(["python", "modelo/prediction.py"], stdout=subprocess.PIPE)
-    output = test.communicate()[0]
+    response = run_yolov5_detection(local_filename)
 
     tiempos = []
     # Asegúrate de que la ruta al archivo tiempos_goles.txt sea la correcta
-    with open('modelo/tiempos_goles.txt') as f:
+    with open('../yolov8/ultralytics/yolo/v8/segment/detection_times.txt') as f:
         tiempos = [line.strip() for line in f.readlines()]
 
-    return {'prediction': tiempos}
+    return {'prediction': response}
--- api/predict.py
+++ api/predict.py
@@ -1,15 +1,44 @@
-# prediction.py
-# Este es un script de predicción simulado que simplemente escribe tiempos de eventos ficticios en un archivo.
+import subprocess
+import cv2
 
-# Simulando tiempos de eventos (por ejemplo, goles) en formato "Gol HH:MM:SS"
-eventos_simulados = [
-    "Gol 00:02:10",
-    "Gol 00:05:00"
-]
+def run_yolov5_detection(source):
+    command = [
+        "python", "../yolov8/ultralytics/yolo/v8/segment/predict_detect.py",
+        "model=../yolov8/ultralytics/yolo/v8/segment/best2.pt",
+        f"source={source}"
 
-# Escribir los eventos simulados en un archivo de texto
-with open('tiempos_goles.txt', 'w') as f:
-    for evento in eventos_simulados:
-        f.write(f"{evento}\n")
+    ]
 
-print("Predicciones simuladas generadas con éxito.")
+
+    try:
+        # Run the YOLOv5 detection command
+        subprocess.run(command, check=True)
+        return "proceso_ejecutado"
+
+    except subprocess.CalledProcessError as e:
+        return f"Error executing YOLOv5 detection: {e}"
+
+
+if __name__ == "__main__":
+    # Specify the path to the YOLOv5 weights file (yolov5s.pt)
+    #pesos manual en step 1
+    #yolov5_weights_path = "../yolov5/runs/train/exp/weights/best.pt"
+
+    # Specify the source for detection (e.g., webcam: 0 or video file: "path/to/video.mp4")
+    detection_source = "../datasets/pcb_dataset/images/val2023/11_missing_hole_02.jpg"
+
+    # Run YOLOv5 detection
+    run_yolov5_detection(detection_source)
+
+    #breakpoint()
+    #cv2.imshow('Imagen', image_final)
+    #breakpoint()
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+#python detect.py --weights yolov5s.pt --source 0
+#python detect.py --weights runs/train/exp8/weights/best.pt --source ../datasets/pcb_dataset/images/val2023/11_missing_hole_01.jpg
+#Results saved to ../yolov5/runs/detect/exp2
+#uvicorn api:app --reload
+# python train.py --batch 16 --cfg cfg/training/custom_yolov7.yaml --epochs 15 --data data/custom.yaml --weights 'yolov7.pt'
+#python train.py --img 640 --epochs 15 --data coco_custom.yaml --weights yolov5s.pt
